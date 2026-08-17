3212c2c2a1d16987335f95141e14e3d6737d97cc
Lee Ian <leeian1011@gmail.com>
2026-08-17 16:20:23 +0800

    feat: introduce new allocated data free function

--- include/linkedlist.h
+++ include/linkedlist.h
@@ -9,6 +9,7 @@ typedef struct t_llist {
 
 void llist_append(t_llist **head, void *data);
 void llist_free(t_llist *head);
+void llist_free_data(t_llist *head, void (*free_data)(void *data));
 size_t llist_len(t_llist *head);
 t_llist *llist_sort(t_llist *head, int (*compare)(t_llist *a, t_llist *b));
 t_llist *llist_rev(t_llist *head);
--- src/linked_list/llist_free.c
+++ src/linked_list/llist_free.c
@@ -10,3 +10,14 @@ void llist_free(t_llist *head) {
 		free(temp);
 	}
 }
+
+void llist_free_data(t_llist *head, void (*free_data)(void *data)) {
+	t_llist *temp = NULL;
+
+	while (head) {
+		temp = head;
+		head = head->next;
+		free_data(temp->data);
+		free(temp);
+	}
+}
